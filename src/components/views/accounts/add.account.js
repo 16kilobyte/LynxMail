a1854d7014e6363bbcbb02ee65c3d00aea1434f2
Eduardo Lourenço <eduardo.lourenco@globalhitss.com.br>
2017-11-07 15:42:38 -0200

    Ajustando de list para scrollview o add account

--- src/components/views/accounts/add.account.js
+++ src/components/views/accounts/add.account.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Platform, StatusBar, FlatList, StyleSheet, Text, View } from "react-native";
+import { Platform, StatusBar, StyleSheet, Text, View, ScrollView, Image } from "react-native";
 import { connect } from 'react-redux';
 import { addAccount } from "../../../actions/account";
 
@@ -12,19 +12,24 @@ class AddAccountView extends Component {
     marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   };
 
+  renderFlatListItem(item) {
+    return (
+      <Text style={Styles.item}>{item.key}</Text>
+    )
+  }
   render() {
     const { navigation } = this.props;
     return (
-      <View style={Styles.container}>
-        <FlatList
-          data={[
-            {key: 'Google'},
-            {key: 'Outlook'},
-            {key: 'Yahoo'}
-          ]}
-          renderItem={({item}) => <Text style={Styles.item}>{item.key}</Text>}
-        />
-      </View>
+      <ScrollView style={Styles.container}>
+        <View style={Styles.item}>
+          <Image resizeMode="contain" style={{width: 200}}
+            source={require('../../images/outlooklogo272x138.png')} />
+        </View>
+        <View style={Styles.item}>
+          <Image resizeMode="contain" style={{width: 200}}
+            source={require('../../images/googlelogo272x92.png')} />
+        </View>
+      </ScrollView>
     )
   }
 }
--- src/components/views/accounts/style.js
+++ src/components/views/accounts/style.js
@@ -3,17 +3,18 @@ import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop:1
+    flex: 1
    },
    item: {
     flex: 1,
     flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 1,
     marginRight:2,
     marginBottom:1,
     marginLeft:2,
-    height: 90,
+    height: 100,
     backgroundColor: '#ffffff'
    },
 })
