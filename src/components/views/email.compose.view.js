dfa4e416d9c21d0841f69d527423f91f3bef2f65
Eduardo Lourenço <dudu.developer@gmail.com>
2017-11-20 21:30:55 -0200

    add rich text editor no compose mail

--- src/components/views/email.compose.view.js
+++ src/components/views/email.compose.view.js
@@ -11,6 +11,8 @@ import { NavigationActions } from 'react-navigation';
 import { Card, Button, FormLabel, FormInput } from "react-native-elements";
 import IconRight from 'react-native-vector-icons/MaterialIcons';
 import IconLeft from 'react-native-vector-icons/MaterialCommunityIcons';
+import {RichTextEditor, RichTextToolbar} from 'react-native-zss-rich-text-editor';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
 
 import Styles from '../style/email.compose.style';
 
@@ -33,6 +35,32 @@ class EmailComposeView extends Component {
       </TouchableOpacity>
     )
   });
+  
+  constructor(props) {
+    super(props);
+    this.getHTML = this.getHTML.bind(this);
+    this.setFocusHandlers = this.setFocusHandlers.bind(this);
+  }
+
+  onEditorInitialized() {
+    this.setFocusHandlers();
+    this.getHTML();
+  }
+
+  async getHTML() {
+    const titleHtml = await this.richtext.getTitleHtml();
+    const contentHtml = await this.richtext.getContentHtml();
+    //alert(titleHtml + ' ' + contentHtml)
+  }
+
+  setFocusHandlers() {
+    this.richtext.setTitleFocusHandler(() => {
+      //alert('title focus');
+    });
+    this.richtext.setContentFocusHandler(() => {
+      //alert('content focus');
+    });
+  }
 
   render() {
     return (
@@ -42,7 +70,17 @@ class EmailComposeView extends Component {
         <FormInput placeholder="Email address..." />
         <FormLabel>Assunto:</FormLabel>
         <FormInput placeholder="Escreva o assunto do eMail" />
-        
+        <RichTextEditor
+              ref={(r)=>this.richtext = r}
+              style={styles.richText}
+              initialTitleHTML={'Logo'}
+              initialContentHTML={'<b>Assinatura teste</b><p>Empresa Andrios</p>'}
+              editorInitializedCallback={() => this.onEditorInitialized()}
+          />
+          <RichTextToolbar
+            getEditor={() => this.richtext}
+          />
+        {Platform.OS === 'ios' && <KeyboardSpacer/>}
     </View>
     )
   }
